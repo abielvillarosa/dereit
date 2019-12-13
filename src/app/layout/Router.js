8587e00af39182390efe74028b47ae6a94f8cec9
Abiel Villarosa <john_abiel@yahoo.com>
2019-12-13 16:04:46 -0500

    Fix router issue

--- src/app/layout/Router.js
+++ src/app/layout/Router.js
@@ -1,22 +1,11 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Home from './../Home'
-// import RestoHome from './../RestoHome'
-// import CustomerHome from './../CustomerHome'
-// import CustomerHome2 from './../CustomerHome2'
-// import Gallery from './../Gallery'
-// import PostDetails from './../PostDetails'
 
 
 const Router = () => (
   <Switch>
-    <Route exact path='/' component={Home}/>
-    {/* <Route path='/strodApp/RestoHome' component={RestoHome}/>
-    <Route path='/strodApp/CustomerHome' component={CustomerHome}/>
-    <Route path='/strodApp/CustomerHome2' component={CustomerHome2}/>
-    <Route path='/strodApp/Gallery' component={Gallery}/> */}
-    {/* <Route path='/swop/swopbooking' component={SwopBooking}/>
-    <Route path='/swop/postdetails' render={(props) => <PostDetails {...props} isAuthed={true} />} component={PostDetails}/> */}
+    <Route exact path='/dereit' component={Home}/>
   </Switch>
 )
 export default Router
