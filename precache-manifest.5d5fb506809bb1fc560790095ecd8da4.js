9239bbbc8b6abf5e0bafe345e53da3c6f43a2acb
Abiel Villarosa <john_abiel@yahoo.com>
2019-12-13 16:00:54 -0500

    Updates

---
+++ precache-manifest.5d5fb506809bb1fc560790095ecd8da4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73d7af49b34f149a9154e0b8aad1ade8",
+    "url": "/dereit/index.html"
+  },
+  {
+    "revision": "1a3adb8672ec2ee6e9b9",
+    "url": "/dereit/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a2cddc5f0835b2c7c0d1",
+    "url": "/dereit/static/js/2.33e48ace.chunk.js"
+  },
+  {
+    "revision": "1a3adb8672ec2ee6e9b9",
+    "url": "/dereit/static/js/main.c7f606de.chunk.js"
+  },
+  {
+    "revision": "86f843f2b0a2d1107377",
+    "url": "/dereit/static/js/runtime-main.cbd4f035.js"
+  },
+  {
+    "revision": "0c52cd9d113c4e379944df2e1dc39ddb",
+    "url": "/dereit/static/media/banner.0c52cd9d.png"
+  },
+  {
+    "revision": "ea60e965fd1162da9142d6858f594724",
+    "url": "/dereit/static/media/dereit-logo.ea60e965.PNG"
+  }
+]);
